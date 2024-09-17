63a29b028711e505ecc231e7424aa25422258e23
Aleix <aleix.povedano@cirvianum.cat>
2024-09-17 13:02:04 +0200

    Inicialització

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aleix Page</title>
+        <style>
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 90%;
+            }
+        </style>
+    </head>
+    <body>
+        <h1><?php echo "Aleix"; ?></h1>
+        <p>
+        <?php
+            echo "Benvingut Aleix";
+            echo "Today is " . date("l");
+            echo "Espero que t'agradi";
+        ?>
+        </p>
+        <img src="hello1.gif" alt="Hola" class="center">
+    </body>
+</html>
